71ecb41941215d2857941a56c7b60a9af65ffa81
Isaac Chung <69920967+ichung08@users.noreply.github.com>
2025-09-29 12:56:23 -0700

    fix: route generator functions (#18)

--- cadwyn/schema_generator.go
+++ cadwyn/schema_generator.go
@@ -110,7 +110,7 @@ type FieldWrapper struct {
 	goType       string
 	jsonTag      string
 	isRequired   bool
-	defaultValue interface{} // TODO: Use for default field values
+	defaultValue interface{} // Default value for field (used during schema generation)
 	isDeleted    bool
 }
 
